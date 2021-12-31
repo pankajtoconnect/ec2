fa2909a3e845a4682a2bdf7e6e410c0069097755
pankajtoconnect <31314519+pankajtoconnect@users.noreply.github.com>
2021-12-31 16:24:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  aws_access_key_id = AKIATXQFL5PVY4BSDNLQ
-  aws_secret_access_key = 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
-
+  shared_credentials_file = "/home/pankaj/.aws/credentials"
+  profile = "default"
 }
