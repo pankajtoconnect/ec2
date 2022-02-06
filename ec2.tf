74e2e2b02d3c3ba986b8bd98f8e8e10541212e8d
pankajtoconnect <31314519+pankajtoconnect@users.noreply.github.com>
2022-02-06 22:23:35 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
+  shared_credentials_file = "/home/pankaj/.aws/credentials"
+  profile                 = "default"
 }
 resource "aws_instance" "web" {
   ami           = "ami-0002bdad91f793433"
