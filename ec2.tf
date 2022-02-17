f7db10db8c32b60fd4c2395184798dad1c9593c7
pankajtoconnect <31314519+pankajtoconnect@users.noreply.github.com>
2022-02-17 22:25:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "web" {
   count = 2
-  ami           = "ami-0002bdad91f793433"
+  ami           = "ami-0c6615d1e95c98aca"
   instance_type = "t2.micro"
 
   tags = {
